b52d6e71f93201f17bebfa504bb23340bcf5a365
Cooper Terrones <cooper.terrones@outlook.com>
2020-11-10 20:44:27 -0700

    Add clean up

--- src/Customer.js
+++ src/Customer.js
@@ -5,15 +5,15 @@ class Customer extends User {
     super(id, name, [], roomData, bookingData);
     this.myBookings = [];
     this.totalSpent = 0;
-  }
+  };
 
   getUserBookings(id) {
     const userBookings = this.bookings.filter((booking) => {
       return booking.userID === id;
     })
     this.myBookings = userBookings;
-  }
+  };
 
-}
+};
 
 export default Customer;
